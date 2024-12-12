3527b37be4c028b10bdc963319b298f313723246
Ze40 <miko.r4r@gmail.com>
2024-12-12 12:17:57 +0300

    featch(wordTable): Добавил простую проверку можно ли установить слово в позицию

--- WordTable.cs
+++ WordTable.cs
@@ -40,6 +40,29 @@ namespace cris_cros
             return true;
         }
 
+        public bool IsCanAdd(Word incWord, Word newWord, int x, int y) 
+        {
+            foreach (Word word in words)
+            {
+                if (incWord.Equals(word)) continue;
+                if (word.isVertical == newWord.isVertical)
+                {
+                    if (word.isVertical)
+                    {
+                        if (x == word.x && (y >= word.y || y <= word.y + word.size)) return false;
+                        if (((word.x - 1 == x) || (word.x + 1 == x)) || ((word.y - 1 == y + newWord.size) || (word.y + 1 == y))) return false;
+                    }
+                    else
+                    {
+                        if (y == word.y && (x >= word.x || x <= word.x + word.size)) return false;
+                        if (((word.x - 1 == x+newWord.size) || (word.x + 1 == x)) || ((word.y - 1 == y) || (word.y + 1 == y))) return false;
+                    }
+                }
+                
+            }
+            return true;
+        }
+
         public bool AddWord(string word)
         {
             if (wordCount == 0) { 
@@ -62,18 +85,31 @@ namespace cris_cros
                             if (!insWord.isVertical)
                             {
                                 //проверка возможности вставки
-                                Word newWord = new Word(insWord.x + i, insWord.y - j, word, true);
-                                words.Add(newWord);
-                                wordCount++;
-                                return true;
+                                int newWordX = insWord.x + i;
+                                int newWordY = insWord.y - j;
+
+                                Word newWord = new Word(newWordX, newWordY, word, true);
+
+                                if (IsCanAdd(insWord, newWord, newWordX, newWordY)) {
+                                    words.Add(newWord);
+                                    wordCount++;
+                                    return true;
+                                } 
                             }
                             else
                             {
                                 //проверка возможности вставки
+                                int newWordX = insWord.x + j;
+                                int newWordY = insWord.y + i;
+
                                 Word newWord = new Word(insWord.x + j, insWord.y + i, word, false);
-                                words.Add(newWord);
-                                wordCount++;
-                                return true;
+
+                                if (IsCanAdd(insWord, newWord, newWordX, newWordY))
+                                {
+                                    words.Add(newWord);
+                                    wordCount++;
+                                    return true;
+                                }
                             }
                         }
                     }
